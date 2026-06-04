a9edce2f1affd718dd3dcccb04cba22372ae009f
HirushaMalindra <hirushamalinda2@gmail.com>
2026-06-04 10:48:23 +0530

    Add initial setup for matplotlib plot in power.py

--- Advance Power Ring/power.py
+++ Advance Power Ring/power.py
@@ -1,1 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+p = 78 
+fig, ax = plt.subplots(figsize=(4,4))
+ax.set_facecolor( "black" )
+t = np.linspace(0, 2*np.pi, 300)
+ax.plot(np.cos(t), np.sin(t), color = 'gray' , lw=8, alpha=0.3)
+
+a= np.lin
