0d63c469c05715a8336ad793cdd040525f001c9a
HirushaMalindra <hirushamalinda2@gmail.com>
2026-06-27 09:38:02 +0530

    Fix syntax error in for loop in Scanner.py

--- Biometric Iris Scanner/Scanner.py
+++ Biometric Iris Scanner/Scanner.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 img = np.zeros((500,500,3),dtype=np.unit8)
-for r in range(50,200,20)
+for r in range(50,200,20):
     cv2.circle(img,(250,250),r,(255,255,0),2)
 
 for a in range(0,360,15):
